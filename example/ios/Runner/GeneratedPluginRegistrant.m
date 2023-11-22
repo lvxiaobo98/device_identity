40b35b5139f1708a4af6a44bc2856d3bc7271bd4
lvxiaobo98 <1363761458@qq.com>
2023-11-22 19:05:28 +0800

    更新原插件device_identity 现已更新内部sdk版本，后续需要升级接着升级即可

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<device_identity/DeviceIdentityPlugin.h>)
+#import <device_identity/DeviceIdentityPlugin.h>
+#else
+@import device_identity;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [DeviceIdentityPlugin registerWithRegistrar:[registry registrarForPlugin:@"DeviceIdentityPlugin"]];
+}
+
+@end
